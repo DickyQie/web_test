64844eea5c0255e94218f0e529ca514583474b23
DickyQie <18523978916@163.com>
2017-04-19 11:18:29 +0800

    web

---
+++ project2/js/top.js
@@ -0,0 +1,8 @@
+
+function myTopls(){
+	document.getElementById("cls_sign").style.display="none";
+	/*document.getElementsByName("cls_sign1").Style().color="red";*/
+}
+function myOnmouseleave(){
+	document.getElementById("cls_sign").style.display="inline-block";
+}
